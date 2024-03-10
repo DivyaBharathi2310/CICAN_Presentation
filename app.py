7d8816f3c429abd151d80ecbc3ec48b7cdb3a979
DivyaBharathi2310 <145807952+DivyaBharathi2310@users.noreply.github.com>
2024-03-10 17:09:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -236,23 +236,21 @@ def data_collection():
 
 def chatbot():
     st.title("Interactive Food Drive Assistant")
+    st.markdown(f"**Disclaimer:** This chatbot may provide project specific information, but will mostly provide general answers and serves mostly as a demonstration of an app integrated chatbot.")
     st.write("Ask a question about the Food Drive!")
 
     with Gradient() as gradient:
-        base_model = gradient.get_base_model(base_model_slug="nous-hermes2")
-        new_model_adapter = base_model.create_model_adapter(name="interactive_food_drive_model")
+        new_model_adapter = gradient.get_model_adapter(model_adapter_id=st.secrets['Model_ID'])        
 
-        user_input = st.text_input("Ask your question:")
+        user_input = st.text_input("")
         if user_input and user_input.lower() not in ['quit', 'exit']:
             sample_query = f"### Instruction: {user_input} \n\n### Response:"
-            st.markdown(f"Asking: {sample_query}")
+            #st.markdown(f"Asking: {sample_query}")
+            st.markdown(f"### Response:")
 
             # before fine-tuning
             completion = new_model_adapter.complete(query=sample_query, max_generated_token_count=100).generated_output
-            st.markdown(f"Generated: {completion}")
-
-        # Delete the model adapter after generating the response
-        new_model_adapter.delete()
+            st.markdown(f"{completion}")
         
 # Main App Logic
 def main():
